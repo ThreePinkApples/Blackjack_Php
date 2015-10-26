c624bd8c13642fe81667148777405245b32af202
Øyvind Instefjord <instefjord@outlook.com>
2015-10-26 10:45:11 +0100

    Forgot to revert test values..

--- php/blackjack.php
+++ php/blackjack.php
@@ -586,7 +586,7 @@ function createDeck(){
     $counter = 0;
 
     for($i = 0; $i < $_SESSION['size']; $i++){
-        /*for($j = 1; $j <= 13; $j++){
+        for($j = 1; $j <= 13; $j++){
             $_SESSION['deck'][$counter] = ['color' => 'h', 'value' => $j, 'gameValue' => $j > 10 ? 10 : $j];
             $counter++;
         }
@@ -601,8 +601,8 @@ function createDeck(){
         for($j = 1; $j <= 13; $j++){
             $_SESSION['deck'][$counter] = ['color' => 's', 'value' => $j, 'gameValue' => $j > 10 ? 10 : $j];
             $counter++;
-        }*/
-        for($j = 1; $j <= 13; $j++){
+        }
+        /*for($j = 1; $j <= 13; $j++){
             $_SESSION['deck'][$counter] = ['color' => 'h', 'value' => $j, 'gameValue' => 10];
             $counter++;
         }
@@ -617,7 +617,7 @@ function createDeck(){
         for($j = 1; $j <= 13; $j++){
             $_SESSION['deck'][$counter] = ['color' => 's', 'value' => $j, 'gameValue' => 1];
             $counter++;
-        }
+        }*/
     }
 
     shuffle($_SESSION['deck']);
